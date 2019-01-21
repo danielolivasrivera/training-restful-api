78f65e46218a31fad3e281e131e514ea57f82935
Daniel A Olivas Rivera <Daniel.OlivasRivera@carsarrive.com>
2019-01-20 22:24:16 -0700

    master: Initial commit

---
+++ LibraryAPI/LibraryAPI/Entities/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Entities
+{
+    public class LibraryContext : DbContext
+    {
+        #region Constructor
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+        #endregion
+
+        #region Properties
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        #endregion
+    }
+}
